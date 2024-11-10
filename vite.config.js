7ebcf8c8cfbae10463c60b5b92dc0844a0fe6774
Olie bat <bationo.c@gmail.com>
2024-11-10 22:18:11 +0100

    Refactor build and server configurations

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	build: {
+		outDir: 'dist',
+		assetsDir: 'assets',
+		rollupOptions: {
+			output: {
+				manualChunks: undefined,
+				entryFileNames: 'assets/[name]-[hash].js',
+				chunkFileNames: 'assets/[name]-[hash].js',
+				assetFileNames: 'assets/[name]-[hash].[ext]',
+			},
+		},
+		assetsInlineLimit: 0,
+		manifest: true,
+		ssrManifest: true,
+	},
+	server: {
+		headers: {
+			'Content-Type': 'application/javascript',
+			'Access-Control-Allow-Origin': '*',
+		},
+	},
+	preview: {
+		headers: {
+			'Content-Type': 'application/javascript',
+			'Access-Control-Allow-Origin': '*',
+		},
+	},
+	base: '/',
 })
